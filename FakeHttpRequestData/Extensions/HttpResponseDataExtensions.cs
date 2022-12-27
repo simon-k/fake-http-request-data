2c7bf1e644f3eec8c17b2289b5657dd831aa77da
Simon Kofod <github@kofod.it>
2022-12-27 14:51:28 +0100

    Initial work (#1)

---
+++ FakeHttpRequestData/Extensions/HttpResponseDataExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Azure.Functions.Worker.Http;
+
+namespace FakeHttpRequestData.Extensions;
+
+public static class HttpResponseDataExtensions
+{
+    public static string ReadHttpResponseData(this HttpResponseData response)
+    {
+        var stream = response.Body;
+        if (stream is not MemoryStream)
+        {
+            return string.Empty;
+        }
+
+        stream.Position = 0;
+        using var reader = new StreamReader(stream);
+        return reader.ReadToEnd();
+    }
+}
---
+++ FakeHttpRequestData/FakeHttpResponseData.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.Functions.Worker;
+using System.Net;
+
+namespace FakeHttpRequestData;
+public class FakeHttpResponseData : HttpResponseData
+{
+    public FakeHttpResponseData(FunctionContext functionContext, HttpStatusCode status)
+        : base(functionContext)
+    {
+        StatusCode = status;
+    }
+
+    public override HttpStatusCode StatusCode { get; set; }
+    public override HttpHeadersCollection Headers { get; set; } = new HttpHeadersCollection();
+    public override Stream Body { get; set; } = new MemoryStream();
+    public override HttpCookies Cookies { get; }
+}
